83e0543aca55723c30fdaaaf4c9840da1a40e054
invictus1032 <invictus1032@gmail.com>
2025-04-01 15:45:44 +0700

    fix: add beacon request support by allowing credentials in CORS headers

--- api/log-answers.js
+++ api/log-answers.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'https://www.16personalities.com');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     // Handle OPTIONS preflight request
     if (req.method === 'OPTIONS') {
